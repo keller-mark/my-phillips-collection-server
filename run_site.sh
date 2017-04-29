70e391de22893f6ed81ceefa147fb315d6bc6626
Ubuntu <ubuntu@ip-172-31-18-62.us-west-2.compute.internal>
2017-04-29 02:03:44 +0000

    made bash script to run site in background

---
+++ run_site.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PS_LINE=`sudo ps | grep fire`
+if [ "$PS_LINE" ]; then
+  echo "Process found"
+  PID=`echo "$PS_LINE" | cut -d' ' -f2`
+  sudo kill $PID && echo "Process killed"
+else
+  echo "Process not found"
+fi
+go build && echo "Site built. Starting..." && (sudo ./fire-phillips-data &)
