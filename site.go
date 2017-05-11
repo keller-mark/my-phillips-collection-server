9bd85e1e791045dac3130dd03994a07e11781f54
Ubuntu <ubuntu@ip-172-31-18-62.us-west-2.compute.internal>
2017-05-11 01:40:25 +0000

    Worked on individual works statistics view

--- site.go
+++ site.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-"fmt"
+  "fmt"
+  "bytes"
   "strconv"
   "log"
   "net/http"
@@ -154,6 +155,158 @@ func visualizeLikes(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
   w.Write(b)
 }
 
+func visualizeWorkList(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+  db := DB();
+
+  type WorkListing struct {
+    ID	  string
+    Title string
+  }
+  allWorks := []Work{}
+  db.Find(&allWorks)
+  
+  workListings := []WorkListing{}
+
+  for _, work := range allWorks {
+    workListings = append(workListings, WorkListing{fmt.Sprint(work.ID), work.Title})
+  }
+  b, _ := json.Marshal(workListings)
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(b)
+}
+
+func visualizeWork(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+  db := DB();
+  
+  work_id := params.ByName("work_id")
+  preferences := []Preference{}
+  db.Model(&Preference{}).Where("work_id = ?", work_id).Find(&preferences)
+
+  type WorkStats struct {
+    Likes		int
+    Dislikes		int
+    Net			int
+    
+    LikesByAge		map[string]int
+    DislikesByAge	map[string]int
+    NetByAge		map[string]int
+    
+    LikesByGender	map[string]int
+    DislikesByGender	map[string]int
+    NetByGender		map[string]int
+    
+    LikesByLocation	map[string]int
+    DislikesByLocation	map[string]int
+    NetByLocation	map[string]int
+  }
+
+  workStats := WorkStats{}
+  workStats.LikesByAge = make(map[string]int)
+  workStats.DislikesByAge = make(map[string]int)
+  workStats.NetByAge = make(map[string]int)
+
+  workStats.LikesByGender = make(map[string]int)
+  workStats.DislikesByGender = make(map[string]int)
+  workStats.NetByGender = make(map[string]int)
+
+  workStats.LikesByLocation = make(map[string]int)
+  workStats.DislikesByLocation = make(map[string]int)
+  workStats.NetByLocation = make(map[string]int)
+  
+  for _, pref := range preferences {
+    user := User{}
+    db.First(&user, pref.User_id)
+    var location bytes.Buffer
+    location.WriteString(user.Country)
+    if user.Country == "US" {
+      location.WriteString("-")
+      location.WriteString(user.State)
+    }
+    locationString := location.String()
+
+    if pref.Liked == 1 {
+      workStats.Likes++
+      if _, ok := workStats.LikesByAge[fmt.Sprint(user.Age)]; ok {
+	workStats.LikesByAge[fmt.Sprint(user.Age)]++
+      } else {
+	workStats.LikesByAge[fmt.Sprint(user.Age)] = 1
+      }
+
+      if _, ok := workStats.LikesByGender[fmt.Sprint(user.Gender)]; ok {
+	workStats.LikesByGender[fmt.Sprint(user.Gender)]++
+      } else {
+	workStats.LikesByGender[fmt.Sprint(user.Gender)] = 1
+      }
+      
+      if _, ok := workStats.LikesByLocation[locationString]; ok {
+	workStats.LikesByLocation[locationString]++
+      } else {
+	workStats.LikesByLocation[locationString] = 1
+      }
+    } else {
+      workStats.Dislikes++
+      if _, ok := workStats.DislikesByAge[fmt.Sprint(user.Age)]; ok {
+	workStats.DislikesByAge[fmt.Sprint(user.Age)]++
+      } else {
+	workStats.DislikesByAge[fmt.Sprint(user.Age)] = 1
+      }
+
+      if _, ok := workStats.DislikesByGender[fmt.Sprint(user.Gender)]; ok {
+	workStats.DislikesByGender[fmt.Sprint(user.Gender)]++
+      } else {
+	workStats.DislikesByGender[fmt.Sprint(user.Gender)] = 1
+      }
+      
+      if _, ok := workStats.DislikesByLocation[locationString]; ok {
+	workStats.DislikesByLocation[locationString]++
+      } else {
+	workStats.DislikesByLocation[locationString] = 1
+      }
+    }
+  }
+  workStats.Net = workStats.Likes - workStats.Dislikes
+  for i := 1; i <= 5; i++ {
+    num_likes := 0
+    if _, ok := workStats.LikesByAge[fmt.Sprint(i)]; ok {
+      num_likes = workStats.LikesByAge[fmt.Sprint(i)]
+    }
+    num_dislikes := 0
+    if _, ok := workStats.DislikesByAge[fmt.Sprint(i)]; ok {
+      num_dislikes = workStats.DislikesByAge[fmt.Sprint(i)]
+    }
+    workStats.NetByAge[fmt.Sprint(i)] = num_likes - num_dislikes
+  }
+  for i := 1; i <= 3; i++ {
+    num_likes := 0
+    if _, ok := workStats.LikesByGender[fmt.Sprint(i)]; ok {
+      num_likes = workStats.LikesByGender[fmt.Sprint(i)]
+    }
+    num_dislikes := 0
+    if _, ok := workStats.DislikesByGender[fmt.Sprint(i)]; ok {
+      num_dislikes = workStats.DislikesByGender[fmt.Sprint(i)]
+    }
+    workStats.NetByGender[fmt.Sprint(i)] = num_likes - num_dislikes
+  }
+  for key, val := range workStats.LikesByLocation {
+    workStats.NetByLocation[key] = val
+  }
+  for key, val := range workStats.DislikesByLocation {
+    if _, ok := workStats.NetByLocation[key]; ok {
+      workStats.NetByLocation[key] -= val
+    } else {
+      workStats.NetByLocation[key] = -(val)
+    }
+  }
+  
+  b, err := json.Marshal(workStats)
+  if err != nil {
+    log.Fatal(err)
+  } 
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(b)
+}
+
 func main() { 
   router := httprouter.New()
 
@@ -162,7 +315,11 @@ func main() {
   router.POST("/survey", surveySubmit)
   router.GET("/work/:work_id", work)
   router.POST("/work/preference", newPreference)
+  
   router.GET("/visualize/likes", visualizeLikes)
+  router.GET("/visualize/work-list", visualizeWorkList)
+  router.GET("/visualize/work/:work_id", visualizeWork)
+
   router.ServeFiles("/static/*filepath", http.Dir("./static/"))
 
   // ParseWorksCSV("../../../perm_coll_filtered_20170201.csv")
--- static/js/visualize.js
+++ static/js/visualize.js
@@ -1,11 +1,16 @@
+var workList = [];
+var $select;
+
 routie({
   '/': showChoices,
   '/likes-dislikes': visLikesAndDislikes,
-  '/individual-work': visIndividualWork
+  '/individual-work': visIndividualWork,
+  '/individual-work/?:id': visIndividualWork,
 });
 
 $(document).ready(function() {
   routie('/');
+
 });
 
 
@@ -50,7 +55,6 @@ function visLikesAndDislikes() {
 	i++;
       });
 
-
       $("#visualizationData").html("<table id='visualizationTable' class='table table-striped' width='100%'></table>");
       $("#visualizationTable").DataTable({
 	data: tableDataSet,
@@ -70,17 +74,106 @@ function visLikesAndDislikes() {
 }
 
 
-
-function visIndividualWork() {
+function visIndividualWork(id) {
   hideChoices();
   $("#visualizationTitle").html("By Individual Work");
+  if(workList.length == 0) {
+    (function() {
+      $.ajax({
+	url: "/visualize/work-list",
+	success: function(data) {
+	  workList = data;
+	  visIndividualWork(id);
+	},
+	dataType: "json"
+      });
+    })();
+    return;
+  } else {
+
   $("#visualizationData").html("<select id='visualizationSelect' placeholder='Find a work...'></select>");
-  $("#visualizationSelect").selectize();
+  $select = $("#visualizationSelect").selectize({
+      valueField: 'ID',
+      labelField: 'Title',
+      searchField: 'Title',
+      options: workList,
+      onChange: function(value) {
+	if(!value.length) return;
+	routie("/individual-work/" + value);
+      }
+  });
+  var selectize = $select[0].selectize;
+
+  if(typeof id !== 'undefined') {
+    console.log("id is " + id);
+    selectize.setValue(id);
+    loadWork(id);
+  }
+  }
+}
+
+function loadWork(id) {
+    $.ajax({
+      url: "/work/" + id,
+      success: function(data) {
+	
+	if(!$("#image" + id).length) {
+	  $("#visualizationData").append("<img id='image" + id + "' class='individual-work-img col-xs-6' src='http://www.phillipscollection.org/willo/w/size3/" + data["PhillipsID"] + "w.jpg'>");
+	  
+	  $("#visualizationData").append("<table id='statTable" + id + "' class='col-xs-6'></table>");
+	  loadWorkStats(id);
+	  $("#visualizationData").append("<table id='infoTable" + id + "' class='table table-striped'></table>");
+	  $.each(data, function(key, value) {
+	    if(key != "ID" && key != "PhillipsID" && key != "CreatedAt" && key != "UpdatedAt" && key != "DeletedAt") {
+	      key = key.replace(/([A-Z])/g, ' $1').trim()
+	      $("#infoTable" + id).append("<tr><th>" + key + "</th><td>" + value + "</td></tr>");
+	    }
+	  });
+
+	}
+      }
+    });
+}
+
+var ageStrings = {
+  "1" : "Child (0-12)",
+  "2" : "Teen (13-17)",
+  "3" : "Young Adult (18-30)",
+  "4" : "Adult (30-60)",
+  "5" : "Senior (60+)"
+};
+var genderStrings = {
+  "1" : "Male",
+  "2" : "Female",
+  "3" : "Other"
+};
+
+function loadWorkStats(id) {
   $.ajax({
-    url: "/visualize/work-list",
+    url: '/visualize/work/' + id,
     success: function(data) {
-      
+      console.log(data);
+      $.each(data, function(key, value) {
+  	newKey = key.replace(/([A-Z])/g, ' $1').trim();
+	if(key == "Likes" || key == "Dislikes" || key == "Net") {
+	  $("#statTable" + id).append("<tr><th>" + key + "</th><td>" + value + "</td></tr>");
+	} else {
+	  $("#statTable" + id).append("<tr><th>" + newKey + "</th><td><table id='" + key + id + "' class='innertable'></table></td></tr>");
+	  $.each(value, function(group, num) {
+	    if(num != 0) {
+	      if(key == "LikesByAge" || key == "DislikesByAge" || key == "NetByAge") {
+		$("#" + key + id).append("<tr><th>" + ageStrings[group] + "</th><td>" + num + "</td></tr>");
+	      } else if(key == "LikesByGender" || key == "DislikesByGender" || key == "NetByGender") {
+		$("#" + key + id).append("<tr><th>" + genderStrings[group] + "</th><td>" + num + "</td></tr>");
+	      } else {
+		$("#" + key + id).append("<tr><th>" + group + "</th><td>" + num + "</td></tr>");
+	      }
+	    }
+	  });
+	}
+      });
+
     },
     dataType: "json"
-  })
+  });
 }
